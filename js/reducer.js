a55c41b544ec90d34103c3e83b519bb025a5c686
Danny Lee <dreamer@panix.com>
2020-01-12 22:19:48 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,4 +1,6 @@
-function changeState (state, action) {
+let state;
+
+function changeState (state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 }
@@ -10,15 +12,10 @@ function changeState (state, action) {
 function dispatch (action) {
   state = changeState(state, action)
   render()
-  return state
 }
 
 function render () {
   document.body.textContent = state.count
 }
 
-let state = { count: 0 }
-let action = { type: 'INCREASE_COUNT' }
-
-changeState(state, action)
-render()
+dispatch({ type: '@@INIT' })
